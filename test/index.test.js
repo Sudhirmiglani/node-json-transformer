f83b5c4d2d07afc96a250bb7d70112de3c9321d7
Sudhir Miglani <smiglani@Sudhirs-MBP.lan>
2019-11-21 12:24:53 +0530

    Added travis build

--- test/index.test.js
+++ test/index.test.js
@@ -5,237 +5,239 @@
 
 const transformer = require('../index');
 
-test('object to object transformation', () => {
+describe('test transformations', () => {
 
-    const data = {
-        x: "xx",
-        y: {
-            z: "zz"
-        }
-    };
+    test('object to object transformation', () => {
 
-    const transformation = {
-        mapping: {
-            item: {
-                a: "x",
-                b: "y.z"
+        const data = {
+            x: "xx",
+            y: {
+                z: "zz"
             }
-        }
-    };
+        };
 
-    const output = transformer.transform(data, transformation);
+        const transformation = {
+            mapping: {
+                item: {
+                    a: "x",
+                    b: "y.z"
+                }
+            }
+        };
 
-    expect(output).toStrictEqual({
-                                     a: "xx",
-                                     b: "zz"
-                                 });
+        const output = transformer.transform(data, transformation);
 
-});
+        expect(output).toStrictEqual({
+                                         a: "xx",
+                                         b: "zz"
+                                     });
+
+    });
 
 
-test('array to array transformation', () => {
-    const data = {
-        x: "xx",
-        y: [{
-            z: "z1"
-        },
-            {
-                z: "z2"
-            }]
-    };
-
-    const transformation = {
-        "mapping": {
-            "item": {
-                a: "x",
-                b: [{
-                    "list": "y",
-                    "item": {
-                        "c": "z"
-                    }
+    test('array to array transformation', () => {
+        const data = {
+            x: "xx",
+            y: [{
+                z: "z1"
+            },
+                {
+                    z: "z2"
                 }]
+        };
+
+        const transformation = {
+            "mapping": {
+                "item": {
+                    a: "x",
+                    b: [{
+                        "list": "y",
+                        "item": {
+                            "c": "z"
+                        }
+                    }]
+                }
             }
-        }
-    };
+        };
 
 
-    const output = transformer.transform(data, transformation);
-    expect(output).toStrictEqual({
-                                     a: "xx",
-                                     b:
-                                         [{
-                                             "c": "z1"
-                                         }, {
-                                             "c": "z2"
-                                         }]
-                                 });
-});
+        const output = transformer.transform(data, transformation);
+        expect(output).toStrictEqual({
+                                         a: "xx",
+                                         b:
+                                             [{
+                                                 "c": "z1"
+                                             }, {
+                                                 "c": "z2"
+                                             }]
+                                     });
+    });
 
 
-test('object to object transformation', () => {
+    test('object to object transformation', () => {
 
-    const data = {
-        x: "xx",
-        y: "yy"
-    };
+        const data = {
+            x: "xx",
+            y: "yy"
+        };
 
-    const transformation = {
-        "mapping": {
-            "item": {
-                a: "x",
-                b: "y",
-                c: "$constantValue"
+        const transformation = {
+            "mapping": {
+                "item": {
+                    a: "x",
+                    b: "y",
+                    c: "$constantValue"
+                }
             }
-        }
-    };
+        };
 
-    const output = transformer.transform(data, transformation);
+        const output = transformer.transform(data, transformation);
 
-    expect(output).toStrictEqual({
-                                     a: "xx",
-                                     b: "yy",
-                                     c: "constantValue"
-                                 });
+        expect(output).toStrictEqual({
+                                         a: "xx",
+                                         b: "yy",
+                                         c: "constantValue"
+                                     });
 
 
-});
+    });
 
-test('Array to object transformation', () => {
+    test('Array to object transformation', () => {
 
-    const data = {
-        x: {
-            x1: [{
-                z: "23"
-            }, {
-                z: "45"
-            }]
-        },
-        y: "yy"
-    };
+        const data = {
+            x: {
+                x1: [{
+                    z: "23"
+                }, {
+                    z: "45"
+                }]
+            },
+            y: "yy"
+        };
 
-    const transformation = {
-        "mapping": {
-            "item": {
-                a: {
-                    "flat": "x.x1#eq(1)",
-                    "item": {
-                        "c": "z"
-                    }
-                },
-                b: "y"
+        const transformation = {
+            "mapping": {
+                "item": {
+                    a: {
+                        "flat": "x.x1#eq(1)",
+                        "item": {
+                            "c": "z"
+                        }
+                    },
+                    b: "y"
+                }
             }
-        }
-    };
+        };
 
-    const output = transformer.transform(data, transformation);
+        const output = transformer.transform(data, transformation);
 
-    expect(output).toStrictEqual({
-                                     a: {
-                                         c: "45"
-                                     },
-                                     b: "yy"
-                                 });
+        expect(output).toStrictEqual({
+                                         a: {
+                                             c: "45"
+                                         },
+                                         b: "yy"
+                                     });
 
 
-});
+    });
 
-test('Object to Array transformation', () => {
+    test('Object to Array transformation', () => {
 
-    const data = {
-        x: {
-            x1: "123"
-        },
-        y: 456,
-        z: "abc"
-    };
+        const data = {
+            x: {
+                x1: "123"
+            },
+            y: 456,
+            z: "abc"
+        };
 
-    const transformation = {
-        "mapping": {
-            "item": {
-                "arrayItems": {
-                    "objectify": "x",
-                    "item": {
-                        "car": {
-                            "c1": "x1",
-                            "c2": "$abc"
+        const transformation = {
+            "mapping": {
+                "item": {
+                    "arrayItems": {
+                        "objectify": "x",
+                        "item": {
+                            "car": {
+                                "c1": "x1",
+                                "c2": "$abc"
+                            }
                         }
                     }
                 }
             }
-        }
-    };
+        };
 
 
-    const output = transformer.transform(data, transformation);
+        const output = transformer.transform(data, transformation);
 
-    expect(output).toStrictEqual({
-                                     arrayItems: [{
-                                         car: {
-                                             c1: "123",
-                                             c2: "abc"
-                                         }
-                                     }]
-                                 });
+        expect(output).toStrictEqual({
+                                         arrayItems: [{
+                                             car: {
+                                                 c1: "123",
+                                                 c2: "abc"
+                                             }
+                                         }]
+                                     });
 
 
-});
+    });
 
-test('Type conversion', () => {
+    test('Type conversion', () => {
 
-    const data = {
-        x: "123",
-        y: 456,
-        z: "abc"
-    };
+        const data = {
+            x: "123",
+            y: 456,
+            z: "abc"
+        };
 
-    const transformation = {
-        "mapping": {
-            "item": {
-                a: "x(NUMBER)",
-                b: "y(STRING)",
-                c: "z(UPPER)"
+        const transformation = {
+            "mapping": {
+                "item": {
+                    a: "x(NUMBER)",
+                    b: "y(STRING)",
+                    c: "z(UPPER)"
+                }
             }
-        }
-    };
+        };
 
-    const output = transformer.transform(data, transformation);
+        const output = transformer.transform(data, transformation);
 
-    expect(output).toStrictEqual({
-                                     a: 123,
-                                     b: "456",
-                                     c: "ABC"
-                                 });
+        expect(output).toStrictEqual({
+                                         a: 123,
+                                         b: "456",
+                                         c: "ABC"
+                                     });
 
 
-});
+    });
 
-test('Test operations', () => {
+    test('Test operations', () => {
 
-    const data = {
-        x: "RED",
-        y: "Jude"
-    };
+        const data = {
+            x: "RED",
+            y: "Jude"
+        };
 
-    const transformation = {
-        "config": {
-            "dependentMap": {
-                "RED": "stop",
-                "GREEN": "go"
+        const transformation = {
+            "config": {
+                "dependentMap": {
+                    "RED": "stop",
+                    "GREEN": "go"
+                }
+            },
+            "mapping": {
+                "item": {
+                    a: "x{DEPENDS}"
+                }
             }
-        },
-        "mapping": {
-            "item": {
-                a: "x{DEPENDS}"
-            }
-        }
-    };
-    const output = transformer.transform(data, transformation);
+        };
+        const output = transformer.transform(data, transformation);
 
-    expect(output).toStrictEqual({
-                                     a: "stop"
-                                 });
+        expect(output).toStrictEqual({
+                                         a: "stop"
+                                     });
 
 
+    });
 });
-
 
--- test/utilities.test.js
+++ test/utilities.test.js
@@ -4,75 +4,84 @@
 
 const {getObjectOrArrayFromStringKey, isNonEmptyArray, isNumber} = require('../utils/utilities');
 
-test('get object by path from nested object', () => {
+describe('test getObjectOrArrayFromStringKey', () => {
 
-    let obj = {
-        a: {
-            b : 123
-        }
-    };
-    expect(getObjectOrArrayFromStringKey( "a.b", obj)).toBe(123);
 
+    test('get object by path from nested object', () => {
+
+        let obj = {
+            a: {
+                b: 123
+            }
+        };
+        expect(getObjectOrArrayFromStringKey("a.b", obj)).toBe(123);
+
+    });
+
+    test('get object by path when path is empty', () => {
+
+        let obj = {
+            a: {
+                b: 123
+            }
+        };
+        expect(getObjectOrArrayFromStringKey("", obj)).toStrictEqual(obj);
+
+    });
+
+    test('get object when path is invalid', () => {
+
+        let obj = {
+            a: {
+                b: 123
+            }
+        };
+        expect(getObjectOrArrayFromStringKey("a.b.c.d", obj)).toBe(undefined);
+
+    });
+
+    test('get array by path from nested object', () => {
+
+        let obj = {
+            a: {
+                b: [{
+                    x: 123
+                }]
+            }
+        };
+        expect(getObjectOrArrayFromStringKey("a.b.x", obj)).toStrictEqual([123]);
+
+    });
 });
 
-test('get object by path when path is empty', () => {
+describe('test getObjectOrArrayFromStringKey', () => {
 
-    let obj = {
-        a: {
-            b : 123
-        }
-    };
-    expect(getObjectOrArrayFromStringKey( "", obj)).toStrictEqual(obj);
+    test('check is array empty - empty array', () => {
 
+        expect(isNonEmptyArray([])).toBe(false);
+
+    });
+
+    test('check is array empty - non empty array', () => {
+
+        expect(isNonEmptyArray([1])).toBe(true);
+
+    });
 });
 
-test('get object when path is invalid', () => {
+describe('test isNumber', () => {
 
-    let obj = {
-        a: {
-            b : 123
-        }
-    };
-    expect(getObjectOrArrayFromStringKey( "a.b.c.d", obj)).toBe(undefined);
+    test('check is number - number passed', () => {
 
+        expect(isNumber(1)).toBe(true);
+
+    });
+
+
+    test('check is number - string passed', () => {
+
+        expect(isNumber("abc")).toBe(false);
+
+    });
 });
-
-test('get array by path from nested object', () => {
-
-    let obj = {
-        a: {
-            b : [{
-                x : 123
-            }]
-        }
-    };
-    expect(getObjectOrArrayFromStringKey( "a.b.x", obj)).toStrictEqual([123]);
-
-});
-
-test('check is array empty - empty array', () => {
-
-    expect(isNonEmptyArray([])).toBe(false);
-
-});
-
-test('check is array empty - non empty array', () => {
-
-    expect(isNonEmptyArray([1])).toBe(true);
-
-});
-
-test('check is number - number passed', () => {
-
-    expect(isNumber(1)).toBe(true);
-
-});
-
-
-test('check is number - string passed', () => {
-
-    expect(isNumber("abc")).toBe(false);
-
-});
-
 
